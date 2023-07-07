163aec0075ac2e9ef058c949b612d9c826276432
semphoenix <rodriguezs2021@gmail.com>
2023-07-07 12:41:05 -0700

    Added CSS Styling

--- lifetracker-ui/src/components/Home/Home.jsx
+++ lifetracker-ui/src/components/Home/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Home.css";
+import logo from "../../assets/logo.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page">
+      <div className="home-container">
+        <div className="header-container">
+          <h1>Welcome Back!</h1>
+        </div>
+        <span>
+          <img src={logo} alt="Logo" />
+        </span>
+      </div>
+      <div className="description">
+        <p>Take back your life. Quantify and view your activity.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
